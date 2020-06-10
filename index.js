f24101e8a9b7a2081cee238fdd6ce5e5c10fbc59
Tushar Chandra <me@tusharc.dev>
2020-06-09 19:51:35 -0500

    Add sleep to node

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function getLATimesPuzzle(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitFor(1000);
+  await page.waitFor(2000);
 
   const puzzle = await page.evaluate(() => {
     let labels = [];
